6965ffedf53f81160ad2887462c16033190ced08
bewakes <bewakepandey@gmail.com>
2020-03-01 20:45:59 +0545

    Add docker configs

--- bewakes/settings.py
+++ bewakes/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'bewakes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME', 'postgres'),
+        'USER': os.environ.get('DATABASE_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'postgres'),
+        'PORT': os.environ.get('DATABASE_PORT', '5432'),
+        'HOST': os.environ.get('DATABASE_HOST', 'db'),
     }
 }
 
